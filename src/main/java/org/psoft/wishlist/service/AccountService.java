2f7d2c2e4d420d7a87c3eb529f84924ffb205462
John Petrocik <john@petrocik.net>
2018-11-20 20:09:45 -0800

    Added phone number cleaner

--- src/main/java/org/psoft/wishlist/service/AccountService.java
+++ src/main/java/org/psoft/wishlist/service/AccountService.java
@@ -1,5 +1,8 @@
 package org.psoft.wishlist.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpSession;
 
 import org.psoft.wishlist.dao.UserDao;
@@ -19,7 +22,8 @@ public class AccountService {
 	PlivoClient plivoClient;
 
 	public String sendMFAMessage(String phone) throws Exception {
-		return accountDao.generateMFAToken(phone);
+		return accountDao.generateMFAToken(
+				cleanPhone(phone));
 	}
 
 	public String validatedMFAMessage(String token, String code) {
@@ -40,6 +44,21 @@ public class AccountService {
 
 	private void authenicateUser(WishlistUser wishListUser, HttpSession session) {
 		session.setAttribute("user", wishListUser);
+	}
+
+	String cleanPhone(String rawPhone) {
+		StringBuilder phone = new StringBuilder();
+		Pattern p = Pattern.compile("\\d+");
+		Matcher m = p.matcher(rawPhone);
+		while (m.find()) {
+		  phone.append(m.group());
+		}
+
+		if (phone.charAt(0) != '1') {
+			phone.insert(0, '1');
+		}
+
+		return phone.toString();
 	}
 
 }
